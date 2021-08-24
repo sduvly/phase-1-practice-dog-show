8a8e9220224cc15e831a8bec9c6d6293c552137e
sduvly <sduvly@gmail.com>
2021-08-24 13:34:51 -0400

    commit tree c767ed8e26f2ed36d7bcf553594aa017151da8e4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-})
+document.addEventListener("DOMContentLoaded", () => {
+fetch("http://localhost:3000/dogs")
+.then(resp => resp.json())
+.then(data => { data.forEach(dogs => renderList(dogs))
+});
+})    
+function renderList(dogs){
+    const className = document.querySelector(".padding")
+    const tr = document.querySelector("tr[1]")
+    
+  }
